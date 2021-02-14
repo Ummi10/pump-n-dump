a26b4d221aa526672b274433412b57108affc8b1
Ummi10 <boxhand1@gmail.com>
2021-02-14 14:16:42 +0200

    Get data from internet instead

--- json_parse_symbols.py
+++ json_parse_symbols.py
@@ -1,25 +1,27 @@
 import json
-#TODO: change to API request : https://api.binance.com/api/v3/ticker/price
+import urllib.request
+# TODO: change to API request : https://api.binance.com/api/v3/ticker/price
 
-file = open("trading_symbol_list.txt", "r")
-json_data = json.loads(file.read())
+URL = "https://api.binance.com/api/v3/ticker/price"
 
-
-
+response = urllib.request.urlopen(URL)
+json_data = json.loads(response.read())
 
 trading_pairs = []
 # Get markets/trading pairs
-for index in range(0,len(json_data)):
+for index in range(0, len(json_data)):
     if json_data[index]['symbol'][-3:] == "BTC":
         trading_pairs.append(json_data[index]['symbol'])
 json_data[index]['symbol']
 
+print("AVAILABLE MARKETS: ")
+for market in trading_pairs:
+    print(market)
 
-
-while(1) :
+while(True):
     chosen_market = ""
     input_trading = input("Input market name: ")
-    
+
     for symbol in trading_pairs:
         chosen_market = ""
         if symbol[:-3] == input_trading.upper():
@@ -28,9 +30,5 @@ while(1) :
             break
     if chosen_market == "":
         print("Market not found")
-    
-        #FOUND MARKET. Start trading logic
-        
 
-#print(trading_pairs)
-ll
+        # FOUND MARKET. Start trading logic
